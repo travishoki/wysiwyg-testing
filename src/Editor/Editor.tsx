2d696aef3086131dc80f4cde9211d42a15f5c8bc
Travis Hoki <travis.hoki@gmail.com>
2023-07-31 15:09:32 -0600

    moving stuff around

--- src/Editor/Editor.tsx
+++ src/Editor/Editor.tsx
@@ -5,7 +5,7 @@ import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
 
-import { OnChangePlugin } from "./plugins/OnChangePlugin.ts";
+import { OnChangePlugin } from "../plugins/OnChangePlugin.ts";
 import { Placeholder } from "./Placeholder/Placeholder.tsx";
 
 export const Editor = () => {
