9eb83190bd0e8ba84da5742563ecbdc2cec3038d
Travis Hoki <travis.hoki@gmail.com>
2023-07-31 10:41:07 -0600

    basic LexicalComposer

--- src/Composer/Composer.tsx
+++ src/Composer/Composer.tsx
@@ -1,3 +1,35 @@
 import React from "react";
 
-export const Composer = () => <p>Composer</p>;
+import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+
+const theme = {
+  // Theme styling goes here
+};
+
+// Catch any errors that occur during Lexical updates and log them
+// or throw them as needed. If you don't throw them, Lexical will
+// try to recover gracefully without losing user data.
+function onError(error: Error) {
+  console.error(error);
+}
+
+const initialConfig = {
+  namespace: "MyEditor",
+  theme,
+  onError,
+};
+
+export const Composer = () => {
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <PlainTextPlugin
+        contentEditable={<ContentEditable />}
+        placeholder={<div>Enter some text...</div>}
+        ErrorBoundary={LexicalErrorBoundary}
+      />
+    </LexicalComposer>
+  );
+};
