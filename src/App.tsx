47509b10214c5ffdf7ca4f990507e05b4f6efb99
Travis Hoki <travis.hoki@gmail.com>
2023-08-03 13:02:47 -0600

    output

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 
 import { LexicalComposer } from "@lexical/react/LexicalComposer"
 import { SharedHistoryContext } from "./context/SharedHistoryContext"
@@ -8,16 +8,13 @@ import PlaygroundNodes from "./nodes/PlaygroundNodes"
 import PlaygroundEditorTheme from "./themes/PlaygroundEditorTheme"
 
 import Editor from "./Editor"
+import { Output } from "./Output/Output"
 
 // Catch any errors that occur during Lexical updates and log them
 // or throw them as needed. If you don't throw them, Lexical will
 // try to recover gracefully without losing user data.
 function onError(error: Error) {
   console.error(error)
-}
-
-const onSubmit = (value: string) => {
-  console.log(value)
 }
 
 const initialConfig = {
@@ -29,17 +26,23 @@ const initialConfig = {
 
 // ts-prune-ignore-next
 export const App = () => {
+  const [output, setOutput] = useState<string>()
+
   return (
-    <LexicalComposer initialConfig={initialConfig}>
-      <SharedHistoryContext>
-        <TableContext>
-          <SharedAutocompleteContext>
-            <div className="editor-shell">
-              <Editor onSubmit={onSubmit} />
-            </div>
-          </SharedAutocompleteContext>
-        </TableContext>
-      </SharedHistoryContext>
-    </LexicalComposer>
+    <>
+      <LexicalComposer initialConfig={initialConfig}>
+        <SharedHistoryContext>
+          <TableContext>
+            <SharedAutocompleteContext>
+              <div className="editor-shell">
+                <Editor onSubmit={setOutput} />
+              </div>
+            </SharedAutocompleteContext>
+          </TableContext>
+        </SharedHistoryContext>
+      </LexicalComposer>
+
+      <Output output={output} />
+    </>
   )
 }
---
+++ src/Output/Output.tsx
@@ -0,0 +1,19 @@
+import * as React from "react"
+import "./Output.css"
+
+export const Output = ({ output }: OutputProps) => {
+  if (!output) return null
+
+  return (
+    <div className="output-shell">
+      <div className="output-content">{output}</div>
+      <div className="output-content">
+        <div dangerouslySetInnerHTML={{ __html: output }} />
+      </div>
+    </div>
+  )
+}
+
+type OutputProps = {
+  output: string
+}
