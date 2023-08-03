b10d9c30645604f89e391f709145f24ec2bfc26e
Travis Hoki <travis.hoki@gmail.com>
2023-08-03 11:07:42 -0600

    merge field styling

--- src/nodes/HokiComponent.tsx
+++ src/nodes/HokiComponent.tsx
@@ -2,10 +2,15 @@ import * as React from "react"
 import { Suspense } from "react"
 import "./HokiComponent.css"
 
+import ImageFile from "../images/icons/file-image.svg"
+import ImageClose from "../images/icons/close.svg"
+
 export default function HokiComponent(): JSX.Element {
   return (
     <Suspense fallback={null}>
+      <img alt="icon" className="svg" height="15" src={ImageFile} width="15" />
       <>Hoki was here!</>
+      <img alt="close" className="svg" height="15" src={ImageClose} width="15" />
     </Suspense>
   )
 }
