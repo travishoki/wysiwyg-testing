10f6ac4e9bfdcff8ca6c0730515de80196b73f2f
Travis Hoki <travis.hoki@gmail.com>
2023-08-15 14:27:00 -0600

    break out component

---
+++ src/plugins/ComponentPickerPlugin/ComponentPickerMenuItem.tsx
@@ -0,0 +1,39 @@
+import * as React from "react"
+import { ComponentPickerOption } from "./ComponentPickerOption"
+
+type ComponentPickerMenuItemProps = {
+  index: number
+  isSelected: boolean
+  onClick: () => void
+  onMouseEnter: () => void
+  option: ComponentPickerOption
+}
+
+export function ComponentPickerMenuItem({
+  index,
+  isSelected,
+  onClick,
+  onMouseEnter,
+  option,
+}: ComponentPickerMenuItemProps) {
+  let className = "item"
+  if (isSelected) {
+    className += " selected"
+  }
+  return (
+    <li
+      key={option.key}
+      tabIndex={-1}
+      className={className}
+      ref={option.setRefElement}
+      role="option"
+      aria-selected={isSelected}
+      id={"typeahead-item-" + index}
+      onMouseEnter={onMouseEnter}
+      onClick={onClick}
+    >
+      {option.icon}
+      <span className="text">{option.title}</span>
+    </li>
+  )
+}
--- src/plugins/ComponentPickerPlugin/index.tsx
+++ src/plugins/ComponentPickerPlugin/index.tsx
@@ -17,7 +17,6 @@ import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext
 import { INSERT_HORIZONTAL_RULE_COMMAND } from "@lexical/react/LexicalHorizontalRuleNode"
 import {
   LexicalTypeaheadMenuPlugin,
-  MenuOption,
   useBasicTypeaheadTriggerMatch,
 } from "@lexical/react/LexicalTypeaheadMenuPlugin"
 import { $createHeadingNode, $createQuoteNode } from "@lexical/rich-text"
@@ -34,71 +33,8 @@ import * as ReactDOM from "react-dom"
 import { useModal } from "../../hooks/useModal"
 import { InsertImageDialog } from "../ImagesPlugin"
 import { InsertNewTableDialog, InsertTableDialog } from "../TablePlugin"
-
-class ComponentPickerOption extends MenuOption {
-  // What shows up in the editor
-  title: string
-  // Icon for display
-  icon?: JSX.Element
-  // For extra searching.
-  keywords: Array<string>
-  // TBD
-  keyboardShortcut?: string
-  // What happens when you select this option?
-  onSelect: (queryString: string) => void
-
-  constructor(
-    title: string,
-    options: {
-      icon?: JSX.Element
-      keywords?: Array<string>
-      keyboardShortcut?: string
-      onSelect: (queryString: string) => void
-    },
-  ) {
-    super(title)
-    this.title = title
-    this.keywords = options.keywords || []
-    this.icon = options.icon
-    this.keyboardShortcut = options.keyboardShortcut
-    this.onSelect = options.onSelect.bind(this)
-  }
-}
-
-function ComponentPickerMenuItem({
-  index,
-  isSelected,
-  onClick,
-  onMouseEnter,
-  option,
-}: {
-  index: number
-  isSelected: boolean
-  onClick: () => void
-  onMouseEnter: () => void
-  option: ComponentPickerOption
-}) {
-  let className = "item"
-  if (isSelected) {
-    className += " selected"
-  }
-  return (
-    <li
-      key={option.key}
-      tabIndex={-1}
-      className={className}
-      ref={option.setRefElement}
-      role="option"
-      aria-selected={isSelected}
-      id={"typeahead-item-" + index}
-      onMouseEnter={onMouseEnter}
-      onClick={onClick}
-    >
-      {option.icon}
-      <span className="text">{option.title}</span>
-    </li>
-  )
-}
+import { ComponentPickerMenuItem } from "./ComponentPickerMenuItem"
+import { ComponentPickerOption } from "./ComponentPickerOption"
 
 export function ComponentPickerPlugin(): JSX.Element {
   const [editor] = useLexicalComposerContext()
