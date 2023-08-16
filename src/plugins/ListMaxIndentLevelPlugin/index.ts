dc17f808309280fd1f6c12b5e540cd26336a5999
Travis Hoki <travis.hoki@gmail.com>
2023-08-16 01:45:14 -0600

    breakout helpers

--- src/plugins/ListMaxIndentLevelPlugin/index.ts
+++ src/plugins/ListMaxIndentLevelPlugin/index.ts
@@ -7,64 +7,13 @@
  */
 
 import { useEffect } from "react"
-import { $getListDepth, $isListItemNode, $isListNode } from "@lexical/list"
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext"
-import {
-  $getSelection,
-  $isElementNode,
-  $isRangeSelection,
-  COMMAND_PRIORITY_CRITICAL,
-  ElementNode,
-  INDENT_CONTENT_COMMAND,
-} from "lexical"
-import type { RangeSelection } from "lexical"
+import { COMMAND_PRIORITY_CRITICAL, INDENT_CONTENT_COMMAND } from "lexical"
+import { isIndentPermitted } from "./helpers"
 
 type Props = Readonly<{
   maxDepth: number | null | undefined
 }>
-
-const getElementNodesInSelection = (selection: RangeSelection): Set<ElementNode> => {
-  const nodesInSelection = selection.getNodes()
-
-  if (nodesInSelection.length === 0) {
-    return new Set([
-      selection.anchor.getNode().getParentOrThrow(),
-      selection.focus.getNode().getParentOrThrow(),
-    ])
-  }
-
-  return new Set(nodesInSelection.map((n) => ($isElementNode(n) ? n : n.getParentOrThrow())))
-}
-
-const isIndentPermitted = (maxDepth: number): boolean => {
-  const selection = $getSelection()
-
-  if (!$isRangeSelection(selection)) {
-    return false
-  }
-
-  const elementNodesInSelection: Set<ElementNode> = getElementNodesInSelection(selection)
-
-  let totalDepth = 0
-
-  for (const elementNode of elementNodesInSelection) {
-    if ($isListNode(elementNode)) {
-      totalDepth = Math.max($getListDepth(elementNode) + 1, totalDepth)
-    } else if ($isListItemNode(elementNode)) {
-      const parent = elementNode.getParent()
-
-      if (!$isListNode(parent)) {
-        throw new Error(
-          "ListMaxIndentLevelPlugin: A ListItemNode must have a ListNode for a parent.",
-        )
-      }
-
-      totalDepth = Math.max($getListDepth(parent) + 1, totalDepth)
-    }
-  }
-
-  return totalDepth <= maxDepth
-}
 
 export const ListMaxIndentLevelPlugin = ({ maxDepth }: Props): null => {
   const [editor] = useLexicalComposerContext()
