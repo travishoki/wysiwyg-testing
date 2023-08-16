535566bb43b786a70bfebd236ec0a8e9938534ee
Travis Hoki <travis.hoki@gmail.com>
2023-08-16 10:56:36 -0600

    scss

--- src/Controls/Controls.tsx
+++ src/Controls/Controls.tsx
@@ -1,14 +1,14 @@
 import * as React from "react"
 import { LockButton } from "./LockButton/LockButton"
 import { SubmitButton } from "./SubmitButton/SubmitButton"
-import "./Controls.css"
+import styles from "./Controls.module.scss"
 
 type ControlsProps = {
   onSubmit: (value: string) => void
 }
 
 export const Controls = ({ onSubmit }: ControlsProps) => (
-  <div className="controls">
+  <div className={styles.controls}>
     <SubmitButton onSubmit={onSubmit} />
     <LockButton />
   </div>
