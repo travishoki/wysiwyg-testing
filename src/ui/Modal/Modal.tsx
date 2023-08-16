9bfe0bcbb4bfa0e03664ff00839ad56c253c9bc0
Travis Hoki <travis.hoki@gmail.com>
2023-08-16 02:05:17 -0600

    breakout component

--- src/ui/Modal/Modal.tsx
+++ src/ui/Modal/Modal.tsx
@@ -7,79 +7,10 @@
  */
 
 import * as React from "react"
-import { ReactNode, useEffect, useRef } from "react"
+import { ReactNode } from "react"
 import { createPortal } from "react-dom"
-
+import { PortalImpl } from "./PortalImpl"
 import "./Modal.css"
-
-type PortalImplProps = {
-  children: ReactNode
-  closeOnClickOutside: boolean
-  onClose: () => void
-  title: string
-}
-
-const PortalImpl = ({ onClose, children, title, closeOnClickOutside }: PortalImplProps) => {
-  const modalRef = useRef<HTMLDivElement>(null)
-
-  useEffect(() => {
-    if (modalRef.current !== null) {
-      modalRef.current.focus()
-    }
-  }, [])
-
-  useEffect(() => {
-    let modalOverlayElement: HTMLElement | null = null
-    const handler = (event: KeyboardEvent) => {
-      if (event.keyCode === 27) {
-        onClose()
-      }
-    }
-    const clickOutsideHandler = (event: MouseEvent) => {
-      const target = event.target
-      if (
-        modalRef.current !== null &&
-        !modalRef.current.contains(target as Node) &&
-        closeOnClickOutside
-      ) {
-        onClose()
-      }
-    }
-    const modelElement = modalRef.current
-    if (modelElement !== null) {
-      modalOverlayElement = modelElement.parentElement
-      if (modalOverlayElement !== null) {
-        modalOverlayElement.addEventListener("click", clickOutsideHandler)
-      }
-    }
-
-    window.addEventListener("keydown", handler)
-
-    return () => {
-      window.removeEventListener("keydown", handler)
-      if (modalOverlayElement !== null) {
-        modalOverlayElement?.removeEventListener("click", clickOutsideHandler)
-      }
-    }
-  }, [closeOnClickOutside, onClose])
-
-  return (
-    <div className="Modal__overlay" role="dialog">
-      <div className="Modal__modal" tabIndex={-1} ref={modalRef}>
-        <h2 className="Modal__title">{title}</h2>
-        <button
-          className="Modal__closeButton"
-          aria-label="Close modal"
-          type="button"
-          onClick={onClose}
-        >
-          X
-        </button>
-        <div className="Modal__content">{children}</div>
-      </div>
-    </div>
-  )
-}
 
 type ModalProps = {
   children: ReactNode
