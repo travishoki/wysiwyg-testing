6d2afdf73184684ccaed7dfefae0b3617b20fecb
Travis Hoki <travis.hoki@gmail.com>
2023-08-17 17:13:13 -0600

    breakout hooks

---
+++ src/nodes/Image/LasyImage.hooks.ts
@@ -0,0 +1,14 @@
+const imageCache = new Set()
+
+export const useSuspenseImage = (src: string) => {
+  if (!imageCache.has(src)) {
+    throw new Promise((resolve) => {
+      const img = new Image()
+      img.src = src
+      img.onload = () => {
+        imageCache.add(src)
+        resolve(null)
+      }
+    })
+  }
+}
--- src/nodes/Image/LazyImage.tsx
+++ src/nodes/Image/LazyImage.tsx
@@ -1,19 +1,5 @@
 import React from "react"
-
-const imageCache = new Set()
-
-const useSuspenseImage = (src: string) => {
-  if (!imageCache.has(src)) {
-    throw new Promise((resolve) => {
-      const img = new Image()
-      img.src = src
-      img.onload = () => {
-        imageCache.add(src)
-        resolve(null)
-      }
-    })
-  }
-}
+import { useSuspenseImage } from "./LasyImage.hooks"
 
 type ImageComponentLazyImageProps = {
   altText: string
