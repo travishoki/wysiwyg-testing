cc397c5deae9724718ffe1d4d9e3391d6c5182e0
Travis Hoki <travis.hoki@gmail.com>
2023-08-20 16:32:37 -0600

    composer prefix

--- src/plugins/CodeActionMenuPlugin/CodeActionMenuContainer.helpers.ts
+++ src/plugins/CodeActionMenuPlugin/CodeActionMenuContainer.helpers.ts
@@ -7,7 +7,7 @@ export const getMouseInfo = (event: MouseEvent): getMouseInfoProps => {
   const target = event.target
 
   if (target && target instanceof HTMLElement) {
-    const codeDOMNode = target.closest<HTMLElement>("code.ComposerTheme__code")
+    const codeDOMNode = target.closest<HTMLElement>("code.composer__code")
     const isOutside = !(
       codeDOMNode || target.closest<HTMLElement>("div.code-action-menu-container")
     )
