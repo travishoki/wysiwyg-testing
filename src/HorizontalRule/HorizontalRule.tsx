595e8cc9030d7cf7ce2dbb917cb45d7f62ff20dd
Travis Hoki <travis.hoki@gmail.com>
2023-08-21 00:29:41 -0600

    cleanup

--- src/HorizontalRule/HorizontalRule.tsx
+++ src/HorizontalRule/HorizontalRule.tsx
@@ -2,7 +2,5 @@ import React from "react"
 import styles from "./HorizontalRule.module.scss"
 
 export const HorizontalRule = () => {
-  /* eslint-disable-next-line */
-  console.log("asdf")
   return <hr className={styles.horizontalRule} data-thing="joe" />
 }
