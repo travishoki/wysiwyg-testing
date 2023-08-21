2c577d99022c10153d51e8eb4cb9fa045195e807
Travis Hoki <travis.hoki@gmail.com>
2023-08-21 10:29:10 -0600

    cleanup

--- src/HorizontalRule/HorizontalRule.tsx
+++ src/HorizontalRule/HorizontalRule.tsx
@@ -2,5 +2,5 @@ import React from "react"
 import styles from "./HorizontalRule.module.scss"
 
 export const HorizontalRule = () => {
-  return <hr className={styles.horizontalRule} data-thing="joe" />
+  return <hr className={styles.horizontalRule} />
 }
