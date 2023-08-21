c5423f52881121d89985e7e7fb1a3b0defa50638
Travis Hoki <travis.hoki@gmail.com>
2023-08-21 10:34:41 -0600

    hr style fix

--- src/HorizontalRule/HorizontalRule.tsx
+++ src/HorizontalRule/HorizontalRule.tsx
@@ -1,6 +1,5 @@
 import React from "react"
-import styles from "./HorizontalRule.module.scss"
 
 export const HorizontalRule = () => {
-  return <hr className={styles.horizontalRule} />
+  return <hr />
 }
