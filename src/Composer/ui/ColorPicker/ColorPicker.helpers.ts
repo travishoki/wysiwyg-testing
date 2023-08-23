b0df23b8a710b08994978cf0f2bc30fbd2bad4b1
Travis Hoki <travis.hoki@gmail.com>
2023-08-23 15:08:56 -0600

    ts cleanup

--- src/Composer/ui/ColorPicker/ColorPicker.helpers.ts
+++ src/Composer/ui/ColorPicker/ColorPicker.helpers.ts
@@ -40,7 +40,7 @@ const toHex = (value: string): string => {
 const hex2rgb = (hex: string): RGB => {
   const rbgArr = (
     hex
-      .replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i, (m, r, g, b) => "#" + r + r + g + g + b + b)
+      .replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i, (_m, r, g, b) => "#" + r + r + g + g + b + b)
       .substring(1)
       .match(/.{2}/g) || []
   ).map((x) => parseInt(x, 16))
@@ -89,7 +89,7 @@ const hsv2rgb = ({ h, s, v }: HSV): RGB => {
 }
 
 const rgb2hex = ({ b, g, r }: RGB): string => {
-  return "#" + [r, g, b].map((x) => x.toString(16).padStart(2, "0")).join("")
+  return `#${[r, g, b].map((x) => x.toString(16).padStart(2, "0")).join("")}`
 }
 
 export const transformColor = <M extends keyof Color, C extends Color[M]>(
