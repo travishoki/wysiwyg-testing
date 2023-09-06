28b43ae950986cd624d713c1dfd48d1824beda88
Travis Hoki <travis.hoki@gmail.com>
2023-09-06 15:04:12 -0600

    update test

--- src/Composer/helpers/mergeFields.helpers.test.ts
+++ src/Composer/helpers/mergeFields.helpers.test.ts
@@ -1,7 +1,7 @@
 import { capitalizeWord, formatMergeFieldTitle } from "./mergeFields.helpers"
 
 describe("capitalizeWord", () => {
-  it("capitalizeWord", () => {
+  it("should return capitalized word", () => {
     const str = "one"
     const result = capitalizeWord(str)
     const expectedResult = "One"
@@ -14,6 +14,13 @@ describe("formatMergeFieldTitle", () => {
     const str = "one-two-three"
     const result = formatMergeFieldTitle(str)
     const expectedResult = "One Two Three"
+    expect(result).toEqual(expectedResult)
+  })
+
+  it("should return an empty string", () => {
+    const str: Maybe<string> = null
+    const result = formatMergeFieldTitle(str)
+    const expectedResult = ""
     expect(result).toEqual(expectedResult)
   })
 
