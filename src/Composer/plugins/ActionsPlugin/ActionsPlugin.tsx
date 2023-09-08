fef8d3a261e096905727a823557ef4253132a3c5
Travis Hoki <travis.hoki@gmail.com>
2023-09-08 03:24:23 -0600

    remove @lexical/yjs

--- src/Composer/plugins/ActionsPlugin/ActionsPlugin.tsx
+++ src/Composer/plugins/ActionsPlugin/ActionsPlugin.tsx
@@ -9,12 +9,10 @@
 import React, { useEffect, useState } from "react"
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext"
 import { mergeRegister } from "@lexical/utils"
-import { CONNECTED_COMMAND } from "@lexical/yjs"
-import { $getRoot, $isParagraphNode, COMMAND_PRIORITY_EDITOR } from "lexical"
+import { $getRoot, $isParagraphNode } from "lexical"
 import { useTranslation } from "src/i18n"
 import { useModal } from "../../hooks/useModal"
 import { Icon } from "../../ui/Icon/Icon"
-import { validateEditorState } from "./ActionsPlugin.helpers"
 import styles from "./ActionsPlugin.module.scss"
 import { ShowClearDialog } from "./ShowClearDialog/ShowClearDialog"
 
@@ -32,50 +30,27 @@ export const ActionsPlugin = () => {
       editor.registerEditableListener((editable) => {
         setIsEditable(editable)
       }),
-      editor.registerCommand<boolean>(
-        CONNECTED_COMMAND,
-        () => {
-          return false
-        },
-        COMMAND_PRIORITY_EDITOR,
-      ),
     )
   }, [editor])
 
   useEffect(() => {
-    return editor.registerUpdateListener(
-      ({
-        dirtyElements,
-        // prevEditorState,
-        tags,
-      }) => {
-        // If we are in read only mode, send the editor state
-        // to server and ask for validation if possible.
-        if (
-          !isEditable &&
-          dirtyElements.size > 0 &&
-          !tags.has("historic") &&
-          !tags.has("collaboration")
-        ) {
-          validateEditorState(editor)
-        }
-        editor.getEditorState().read(() => {
-          const root = $getRoot()
-          const children = root.getChildren()
+    return editor.registerUpdateListener(() => {
+      editor.getEditorState().read(() => {
+        const root = $getRoot()
+        const children = root.getChildren()
 
-          if (children.length > 1) {
-            setIsEditorEmpty(false)
+        if (children.length > 1) {
+          setIsEditorEmpty(false)
+        } else {
+          if ($isParagraphNode(children[0])) {
+            const paragraphChildren = children[0].getChildren()
+            setIsEditorEmpty(paragraphChildren.length === 0)
           } else {
-            if ($isParagraphNode(children[0])) {
-              const paragraphChildren = children[0].getChildren()
-              setIsEditorEmpty(paragraphChildren.length === 0)
-            } else {
-              setIsEditorEmpty(false)
-            }
+            setIsEditorEmpty(false)
           }
-        })
-      },
-    )
+        }
+      })
+    })
   }, [editor, isEditable])
 
   return (
