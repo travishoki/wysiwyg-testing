439d2dbfd0287a60ec88c9969f083c7bc0fd308e
Travis Hoki <travis.hoki@gmail.com>
2023-09-11 14:59:43 -0600

    translations

--- src/Composer/plugins/ToolbarPlugin/ButtonRedo/ButtonRedo.tsx
+++ src/Composer/plugins/ToolbarPlugin/ButtonRedo/ButtonRedo.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import classNames from "classnames"
+import { useTranslation } from "src/i18n"
 import { IS_APPLE } from "../../../shared/environment"
 import { IconButton } from "../IconButton/IconButton"
 import stylesToolbarPlugin from "../ToolbarPlugin.module.scss"
@@ -11,13 +12,17 @@ type ButtonRedoProps = {
 }
 
 export const ButtonRedo = ({ canRedo, isEditable, onClick }: ButtonRedoProps) => {
+  const { t } = useTranslation("scenes", { keyPrefix: "composer" })
+
+  const shortcut = IS_APPLE ? "⌘Y" : "Ctrl+Y"
+
   return (
     <button
-      aria-label="Redo"
+      aria-label={t("Redo")}
       className={classNames(stylesToolbarPlugin.toolbarItem)}
       disabled={!canRedo || !isEditable}
       onClick={onClick}
-      title={IS_APPLE ? "Redo (⌘Y)" : "Redo (Ctrl+Y)"}
+      title={`${t("Redo")} (${shortcut})`}
       type="button"
     >
       <IconButton disabled={!canRedo || !isEditable} type="redo" />
--- src/Composer/plugins/ToolbarPlugin/ButtonUndo/ButtonUndo.tsx
+++ src/Composer/plugins/ToolbarPlugin/ButtonUndo/ButtonUndo.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import classNames from "classnames"
+import { useTranslation } from "src/i18n"
 import { IS_APPLE } from "../../../shared/environment"
 import { IconButton } from "../IconButton/IconButton"
 import stylesToolbarPlugin from "../ToolbarPlugin.module.scss"
@@ -11,13 +12,17 @@ type ButtonUndoProps = {
 }
 
 export const ButtonUndo = ({ canUndo, isEditable, onClick }: ButtonUndoProps) => {
+  const { t } = useTranslation("scenes", { keyPrefix: "composer" })
+
+  const shortcut = IS_APPLE ? "⌘Z" : "Ctrl+Z"
+
   return (
     <button
-      aria-label="Undo"
+      aria-label={t("Undo")}
       className={classNames(stylesToolbarPlugin.toolbarItem, stylesToolbarPlugin.spaced)}
       disabled={!canUndo || !isEditable}
       onClick={onClick}
-      title={IS_APPLE ? "Undo (⌘Z)" : "Undo (Ctrl+Z)"}
+      title={`${t("Undo")} (${shortcut})`}
       type="button"
     >
       <IconButton disabled={!canUndo || !isEditable} type="undo" />
