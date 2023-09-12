6d5080d4bb9070a9277d5d50169bddd46ff84ac7
Travis Hoki <travis.hoki@gmail.com>
2023-09-11 18:25:30 -0600

    unit test

--- src/Composer/plugins/ComponentPickerPlugin/ComponentPickerPlugin.helpers.test.ts
+++ src/Composer/plugins/ComponentPickerPlugin/ComponentPickerPlugin.helpers.test.ts
@@ -1,4 +1,15 @@
-import { getHeadingType } from "./ComponentPickerPlugin.helpers"
+import { alignmentTypes } from "../../ui/Icon/types"
+import { getAlignmentType, getHeadingType } from "./ComponentPickerPlugin.helpers"
+
+describe("getAlignmentType", () => {
+  it("should return correct heading by num", () => {
+    expect(getAlignmentType("center")).toEqual("center-align")
+    expect(getAlignmentType("justify")).toEqual("justify-align")
+    expect(getAlignmentType("left")).toEqual("left-align")
+    expect(getAlignmentType("right")).toEqual("right-align")
+    expect(getAlignmentType("" as alignmentTypes)).toEqual("left-align")
+  })
+})
 
 describe("getHeadingType", () => {
   it("should return correct heading by num", () => {
