20f4ea6c8d80a8bf988caa8e4a40839d5832d75d
Travis Hoki <travis.hoki@gmail.com>
2023-09-19 15:10:35 -0600

    check if composer is empty

--- src/Composer/ComposerCustomFunctionHandler/ComposerCustomFunctionHandler.tsx
+++ src/Composer/ComposerCustomFunctionHandler/ComposerCustomFunctionHandler.tsx
@@ -1,6 +1,7 @@
 import { MutableRefObject, useImperativeHandle } from "react"
 import { $generateHtmlFromNodes } from "@lexical/html"
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext"
+import { $getRoot } from "lexical"
 import { MergeField } from "types"
 import { INSERT_MERGE_FIELD_COMMAND } from "../const"
 
@@ -23,6 +24,12 @@ export const ComposerCustomFunctionHandler = ({ composerRef }: MergeFieldHandler
         mergeFieldName: mergeField.name ?? "",
       }
       editor.dispatchCommand(INSERT_MERGE_FIELD_COMMAND, payload)
+    },
+    getIsEmpty(): boolean {
+      const root = $getRoot()
+      const children = root.getChildren()
+
+      return children.length === 0
     },
     onLock() {
       editor.update(() => {
