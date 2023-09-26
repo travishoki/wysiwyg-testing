c4b664a2c1e6eb0780b8f2d3660340345ba243f7
Travis Hoki <travis.hoki@gmail.com>
2023-09-26 09:51:52 -0600

    useless return

--- src/Composer/helpers/getDOMRangeRect.ts
+++ src/Composer/helpers/getDOMRangeRect.ts
@@ -5,20 +5,18 @@
  * LICENSE file in the root directory of this source tree.
  *
  */
+
 export const getDOMRangeRect = (nativeSelection: Selection, rootElement: HTMLElement): DOMRect => {
   const domRange = nativeSelection.getRangeAt(0)
-
-  let rect
 
   if (nativeSelection.anchorNode === rootElement) {
     let inner = rootElement
     while (inner.firstElementChild != null) {
       inner = inner.firstElementChild as HTMLElement
     }
-    rect = inner.getBoundingClientRect()
-  } else {
-    rect = domRange.getBoundingClientRect()
+
+    return inner.getBoundingClientRect()
   }
 
-  return rect
+  return domRange.getBoundingClientRect()
 }
