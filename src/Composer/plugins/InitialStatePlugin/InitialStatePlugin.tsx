2a66f4ad0e844c7652652a06954ec5dcefce2306
Travis Hoki <travis.hoki@gmail.com>
2023-10-11 20:17:19 -0600

    initialStatePlugin

--- src/Composer/plugins/InitialStatePlugin/InitialStatePlugin.tsx
+++ src/Composer/plugins/InitialStatePlugin/InitialStatePlugin.tsx
@@ -15,17 +15,22 @@ export function InitialStatePlugin({ initialState }: InitialStatePluginProps): J
       const root = $getRoot()
       const parser = new DOMParser()
       const dom = parser.parseFromString(initialState, "text/html")
-
       const nodes = $generateNodesFromDOM(editor, dom)
-      const paragraphNode = $createParagraphNode()
-
-      // Add in all of the nodes
-      nodes.forEach((n) => paragraphNode.append(n))
 
       // Clear everything and populate with nodes
       root.clear()
 
-      root.append(paragraphNode)
+      if (nodes.length === 1 && nodes[0].__type === "paragraph") {
+        // Add in the node
+        root.append(nodes[0])
+      } else {
+        const paragraphNode = $createParagraphNode()
+
+        // Add in all of the nodes
+        nodes.forEach((n) => paragraphNode.append(n))
+
+        root.append(paragraphNode)
+      }
     })
   }, [editor, initialState])
 
