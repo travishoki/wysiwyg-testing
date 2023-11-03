235902220ec052f13add7167d522a07c105f7ecc
Travis Hoki <travis.hoki@gmail.com>
2023-11-03 16:10:51 -0600

    update hardcoded mergeField ids

--- src/MergeFieldControls/MergeFieldControls.const.ts
+++ src/MergeFieldControls/MergeFieldControls.const.ts
@@ -5,19 +5,19 @@ const updatedAt = "2023-01-01"
 export const mergeFieldNameArray: MergeField[] = [
   {
     __typename: "MergeField",
-    id: "1",
+    id: "111",
     name: "hoki-1",
     updatedAt,
   },
   {
     __typename: "MergeField",
-    id: "2",
+    id: "222",
     name: "hoki-2",
     updatedAt,
   },
   {
     __typename: "MergeField",
-    id: "3",
+    id: "333",
     name: "hoki-3",
     updatedAt,
   },
