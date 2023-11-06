5c3febeb055ddbdc6f9a50bfa7358bd61462459b
Travis Hoki <travis.hoki@gmail.com>
2023-11-06 14:17:50 -0700

    no need to filter into 1 parent

--- src/Composer/plugins/InitialStatePlugin/InitialStatePlugin.tsx
+++ src/Composer/plugins/InitialStatePlugin/InitialStatePlugin.tsx
@@ -1,7 +1,11 @@
 import { useEffect, useState } from "react"
 import { $generateNodesFromDOM } from "@lexical/html"
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext"
-import { $createParagraphNode, $getRoot, $isParagraphNode } from "lexical"
+import {
+  // $createParagraphNode,
+  $getRoot,
+  $isParagraphNode,
+} from "lexical"
 
 type InitialStatePluginProps = {
   initialState: string
@@ -25,17 +29,8 @@ export function InitialStatePlugin({ initialState }: InitialStatePluginProps): J
         // Clear everything
         root.clear()
 
-        if (nodes.length === 1 && $isParagraphNode(nodes[0])) {
-          // Add in the node
-          root.append(nodes[0])
-        } else {
-          const paragraphNode = $createParagraphNode()
-
-          // Add in all of the nodes
-          nodes.forEach((n) => paragraphNode.append(n))
-
-          root.append(paragraphNode)
-        }
+        // Add in all of the nodes
+        nodes.forEach((n) => root.append(n))
       })
     }
   }, [editor, hasBeenIntialized, initialState, setHasBeenIntialized])
