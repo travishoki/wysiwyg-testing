5657c084c1f1297b971ecf67aae165740ced5d40
Travis Hoki <travis.hoki@gmail.com>
2024-02-01 14:03:52 -0700

    breakout ExtendedTextNode helper

--- src/Composer/nodes/ExtendedTextNode/ExtendedTextNode.tsx
+++ src/Composer/nodes/ExtendedTextNode/ExtendedTextNode.tsx
@@ -1,11 +1,5 @@
-import {
-  $isTextNode,
-  DOMConversion,
-  DOMConversionMap,
-  DOMConversionOutput,
-  SerializedTextNode,
-  TextNode,
-} from "lexical"
+import { DOMConversionMap, SerializedTextNode, TextNode } from "lexical"
+import { patchStyleConversion } from "./ExtendedTextNode.helpers"
 
 export class ExtendedTextNode extends TextNode {
   static getType(): string {
@@ -61,54 +55,6 @@ export class ExtendedTextNode extends TextNode {
       ...super.exportJSON(),
       type: "extended-text",
       version: 1,
-    }
-  }
-}
-
-function patchStyleConversion(
-  originalDOMConverter?: (node: HTMLElement) => DOMConversion | null,
-): (node: HTMLElement) => DOMConversionOutput | null {
-  return (node) => {
-    const original = originalDOMConverter?.(node)
-    if (!original) {
-      return null
-    }
-    const originalOutput = original.conversion(node)
-
-    if (!originalOutput) {
-      return originalOutput
-    }
-
-    const backgroundColor = node.style.backgroundColor
-    const color = node.style.color
-    const fontFamily = node.style.fontFamily
-    const fontWeight = node.style.fontWeight
-    const fontSize = node.style.fontSize
-    const textDecoration = node.style.textDecoration
-
-    return {
-      ...originalOutput,
-      forChild: (lexicalNode, parent) => {
-        const originalForChild = originalOutput?.forChild ?? ((x) => x)
-        const result = originalForChild(lexicalNode, parent)
-        if ($isTextNode(result)) {
-          const style = [
-            backgroundColor ? `background-color: ${backgroundColor}` : null,
-            color ? `color: ${color}` : null,
-            fontFamily ? `font-family: ${fontFamily}` : null,
-            fontWeight ? `font-weight: ${fontWeight}` : null,
-            fontSize ? `font-size: ${fontSize}` : null,
-            textDecoration ? `text-decoration: ${textDecoration}` : null,
-          ]
-            .filter((value) => value != null)
-            .join("; ")
-          if (style.length) {
-            return result.setStyle(style)
-          }
-        }
-
-        return result
-      },
     }
   }
 }
