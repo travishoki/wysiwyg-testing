4e4bf270ebc7611f0b0dc4491996594f8224a8de
Travis Hoki <travis.hoki@gmail.com>
2024-02-01 14:23:35 -0700

    breakout selectAll helpers

--- src/Composer/plugins/SelectAllPlugin/SelectAllPlugin.helpers.ts
+++ src/Composer/plugins/SelectAllPlugin/SelectAllPlugin.helpers.ts
@@ -1,3 +1,4 @@
+import { $createParagraphNode, $getRoot, $isElementNode, LexicalEditor } from "lexical"
 import { IS_APPLE } from "../../shared/environment"
 
 export const controlOrMeta = (metaKey: boolean, ctrlKey: boolean): boolean => {
@@ -10,4 +11,20 @@ export const controlOrMeta = (metaKey: boolean, ctrlKey: boolean): boolean => {
 
 export const isSelectAll = (keyCode: number, metaKey: boolean, ctrlKey: boolean): boolean => {
   return keyCode === 65 && controlOrMeta(metaKey, ctrlKey)
+}
+
+export function $selectAll(editor: LexicalEditor) {
+  const root = $getRoot()
+  const lastNode = root.getLastChild()
+
+  if (!$isElementNode(lastNode) && lastNode) {
+    lastNode.insertAfter($createParagraphNode())
+  }
+
+  root.select(0, root.getChildrenSize())
+
+  const rootElement = editor.getRootElement() as HTMLDivElement
+  rootElement.focus({
+    preventScroll: true,
+  })
 }
--- src/Composer/plugins/SelectAllPlugin/SelectAllPlugin.tsx
+++ src/Composer/plugins/SelectAllPlugin/SelectAllPlugin.tsx
@@ -1,29 +1,6 @@
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext"
-import {
-  $createParagraphNode,
-  $getRoot,
-  $isElementNode,
-  COMMAND_PRIORITY_LOW,
-  KEY_DOWN_COMMAND,
-  LexicalEditor,
-} from "lexical"
-import { isSelectAll } from "./SelectAllPlugin.helpers"
-
-export function $selectAll(editor: LexicalEditor) {
-  const root = $getRoot()
-  const lastNode = root.getLastChild()
-
-  if (!$isElementNode(lastNode) && lastNode) {
-    lastNode.insertAfter($createParagraphNode())
-  }
-
-  root.select(0, root.getChildrenSize())
-
-  const rootElement = editor.getRootElement() as HTMLDivElement
-  rootElement.focus({
-    preventScroll: true,
-  })
-}
+import { COMMAND_PRIORITY_LOW, KEY_DOWN_COMMAND } from "lexical"
+import { $selectAll, isSelectAll } from "./SelectAllPlugin.helpers"
 
 export const SelectAllPlugin = (): null => {
   const [editor] = useLexicalComposerContext()
