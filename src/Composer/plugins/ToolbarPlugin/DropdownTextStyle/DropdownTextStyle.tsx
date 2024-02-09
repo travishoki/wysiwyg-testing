9d16abe103cb87bd2bf238596297e7777046df50
Travis Hoki <travis.hoki@gmail.com>
2024-02-09 15:36:51 -0700

    text formatting translations

--- src/Composer/plugins/ToolbarPlugin/DropdownTextStyle/DropdownTextStyle.tsx
+++ src/Composer/plugins/ToolbarPlugin/DropdownTextStyle/DropdownTextStyle.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import classNames from "classnames"
 import { FORMAT_TEXT_COMMAND, LexicalEditor } from "lexical"
+import { useTranslation } from "src/i18n"
 import { DropDown } from "../../../ui/DropDown/DropDown"
 import stylesDropdown from "../../../ui/DropDown/DropDown.module.scss"
 import { DropDownItem } from "../../../ui/DropDown/DropDownItem/DropDownItem"
@@ -27,16 +28,18 @@ export const DropdownTextStyle = ({
   isSubscript,
   isSuperscript,
 }: DropdownTextStyleProps) => {
+  const { t } = useTranslation("scenes", { keyPrefix: "composer" })
+
   return (
     <DropDown
-      buttonAriaLabel="Formatting options for additional text styles"
+      buttonAriaLabel={t("Formatting options for additional text styles")}
       buttonClassName={classNames(stylesToolbarPlugin.toolbarItem, stylesToolbarPlugin.spaced)}
       buttonIconClassName={classNames(stylesIconDropdown.icon, stylesIcon["dropdown-more"])}
       buttonLabel=""
       disabled={!isEditable}
     >
       <DropDownItem
-        aria-label="Format text with a strikethrough"
+        aria-label={t("Format text with a strikethrough")}
         className={classNames(stylesDropdown.dropdownItem, dropDownActiveClass(isStrikethrough))}
         onClick={() => {
           activeEditor.dispatchCommand(FORMAT_TEXT_COMMAND, "strikethrough")
@@ -44,38 +47,38 @@ export const DropdownTextStyle = ({
         title="Strikethrough"
       >
         <IconDropdown type="strikethrough" />
-        <span className={stylesDropdown.dropdownText}>Strikethrough</span>
+        <span className={stylesDropdown.dropdownText}>{t("Strikethrough")}</span>
       </DropDownItem>
       <DropDownItem
-        aria-label="Format text with a subscript"
+        aria-label={t("Format text with a subscript")}
         className={classNames(stylesDropdown.dropdownItem, dropDownActiveClass(isSubscript))}
         onClick={() => {
           activeEditor.dispatchCommand(FORMAT_TEXT_COMMAND, "subscript")
         }}
-        title="Subscript"
+        title={t("Subscript")}
       >
         <IconDropdown type="subscript" />
-        <span className={stylesDropdown.dropdownText}>Subscript</span>
+        <span className={stylesDropdown.dropdownText}>{t("Subscript")}</span>
       </DropDownItem>
       <DropDownItem
-        aria-label="Format text with a superscript"
+        aria-label={t("Format text with a superscript")}
         className={classNames(stylesDropdown.dropdownItem, dropDownActiveClass(isSuperscript))}
         onClick={() => {
           activeEditor.dispatchCommand(FORMAT_TEXT_COMMAND, "superscript")
         }}
-        title="Superscript"
+        title={t("Superscript")}
       >
         <IconDropdown type="superscript" />
-        <span className={stylesDropdown.dropdownText}>Superscript</span>
+        <span className={stylesDropdown.dropdownText}>{t("Superscript")}</span>
       </DropDownItem>
       <DropDownItem
-        aria-label="Clear all text formatting"
+        aria-label={t("Clear all text formatting")}
         className={stylesDropdown.dropdownItem}
         onClick={clearFormatting}
-        title="Clear text formatting"
+        title={t("Clear text formatting")}
       >
         <IconDropdown type="clear" />
-        <span className={stylesDropdown.dropdownText}>Clear Formatting</span>
+        <span className={stylesDropdown.dropdownText}>{t("Clear text formatting")}</span>
       </DropDownItem>
     </DropDown>
   )
